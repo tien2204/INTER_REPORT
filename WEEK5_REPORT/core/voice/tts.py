4c9182d8053ccbf03269068e7bc37587873017e2
Vũ Hữu Tiến <117528051+tien2204@users.noreply.github.com>
2025-08-04 15:58:31 +0700

    Update tts.py

--- WEEK5_REPORT/core/voice/tts.py
+++ WEEK5_REPORT/core/voice/tts.py
@@ -1,7 +1,7 @@
 import pyttsx3
 import os
 from typing import Optional, List, Dict
-from ..config.settings import settings
+from config.settings import settings
 import logging
 
 logger = logging.getLogger(__name__)
